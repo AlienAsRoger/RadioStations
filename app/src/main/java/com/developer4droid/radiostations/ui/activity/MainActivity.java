9ff38a05598de31c06f10ea6484246bdecd5a21b
AlienAsRoger <sent2roger@gmail.com>
2017-04-13 11:28:14 -0700

    Initial commit

---
+++ app/src/main/java/com/developer4droid/radiostations/ui/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.developer4droid.radiostations.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.developer4droid.radiostations.R;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
